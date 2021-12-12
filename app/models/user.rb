0d393bdcd8e2f89d0b4d03b2b04fbb9187c5d73b
supersay245 <samoa4208@protonmail.com>
2021-12-12 15:09:39 -0600

    post created

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :posts, dependent: :destroy
   validates :email, presence: true, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, length: { minimum: 6 }
