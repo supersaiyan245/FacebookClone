b064ab6d9c34cd469bc1664286bed1c4a75c2ed1
supersay245 <samoa4208@protonmail.com>
2021-12-13 17:33:31 -0600

    parts of Login

---
+++ client/src/screens/Login.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Login(props) {
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+  });
+  const { username, password } = formData;
+  const { handleLogin } = props;
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
