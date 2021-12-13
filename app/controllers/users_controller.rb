fbae77a76aad391203e153d62bb42368620cc4cd
supersay245 <samoa4208@protonmail.com>
2021-12-13 12:42:57 -0600

    signup changed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,6 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :update, :destroy]
+  before_action :set_user, only: :show
 
-  # GET /users
-  def index
-    @users = User.all
-
-    render json: @users
-  end
 
   # GET /users/1
   def show
@@ -26,20 +20,6 @@ class UsersController < ApplicationController
     else
       render json: @user.errors, status: :unprocessable_entity
     end
-  end
-
-  # PATCH/PUT /users/1
-  def update
-    if @user.update(user_params)
-      render json: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /users/1
-  def destroy
-    @user.destroy
   end
 
   private
