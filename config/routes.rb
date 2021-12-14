9b0163dc05030c7c2392fde56ec02cbf6eb080cb
supersay245 <samoa4208@protonmail.com>
2021-12-14 17:02:36 -0600

    verifications work

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  post '/auth/login', to: 'authentication#login'
-  get '/auth/verify', to: 'authentication#verify'
+  post '/auth/login', to: 'authentications#login'
+  get '/auth/verify', to: 'authentications#verify'
   
   resources :users, :only => [:create, :show] 
   resources :users do
