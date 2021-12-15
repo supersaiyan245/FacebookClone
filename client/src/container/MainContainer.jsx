b8f38b6b23098f2e5b8def5ea28242e51def2ae9
supersay245 <samoa4208@protonmail.com>
2021-12-15 05:24:44 -0600

    changes made to container

--- client/src/container/MainContainer.jsx
+++ client/src/container/MainContainer.jsx
@@ -3,21 +3,38 @@ import { Switch, Route, useHistory } from 'react-router-dom';
 import Posts from '../screens/Posts.jsx';
 import PostCreate from '../screens/PostCreate.jsx';
 import PostEdit from '../screens/PostEdit.jsx';
+import { deletePost, postPost } from '../services/post.js';
 
 
-useEffect(() => {
-  const fetchUsers = async () => {
-    const Users = await getAllUsers();
-    setUsers(Users);
+export default function MainContainer({ currentUser }) {
+  const [Posts, setPosts] = useState([]);
+  const [users, setUsers] = useState([]);
+  const history = useHistory();
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const Users = await getAllUsers();
+      setUsers(Users);
+    };
+    fetchUsers();
+  }, []);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const Posts = await getAllPosts();
+      setPosts(Posts);
+    };
+    fetchPosts();
+  }, []);
+
+  const handlePostCreate = async (formData) => {
+    const newPost = await postPost(formData);
+    setPosts((prevState) => [...prevState, newFood]);
+    history.push('/foods');
   };
-  fetchUsers();
-}, []);
 
-useEffect(() => {
-  const fetchPosts = async () => {
-    const Posts = await getAllPosts();
-    setPosts(Posts);
+  const handlePostDelete = async (id) => {
+    await deletePost(id);
+    setPosts((prevState) => prevState.filter((post) => post.id !== id));
   };
-  fetchPosts();
-}, []);
-
+}
