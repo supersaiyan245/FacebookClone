053d0f5adeb220f3604348d15d90aa0b2267f172
supersay245 <samoa4208@protonmail.com>
2021-12-15 04:33:06 -0600

    main container being made

---
+++ client/src/container/MainContainer.jsx
@@ -0,0 +1,5 @@
+import { useState, useEffect } from 'react';
+import { Switch, Route, useHistory } from 'react-router-dom';
+import Posts from '../screens/Posts.jsx';
+import PostCreate from '../screens/PostCreate.jsx';
+import PostEdit from '../screens/PostEdit.jsx';
