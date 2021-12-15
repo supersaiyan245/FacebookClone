cfdeafb97563d195765a99692fe3524f8b30b66b
supersay245 <samoa4208@protonmail.com>
2021-12-15 04:13:04 -0600

    layout work needed

--- client/src/layout/Layout.jsx
+++ client/src/layout/Layout.jsx
@@ -4,5 +4,19 @@ import { Link } from 'react-router-dom';
 
 export default function Layout(currentUser, handleLogout) {
 
-
-
+  return (
+    <div className="gridcontainers">
+       <Link to='/Home'>posts</Link>
+        <h3>MessageMe</h3>
+      {currentUser ? (
+        <div>
+          <p>{currentUser.username}</p>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      ) : (
+          <Link to='/Login'>Login </Link>
+          <Link to='/Register'>Register</Link>
+      )}
+    </div>
+  );
+}
