c6e01f5c837c35155f8103bd12c6fb26f4d84dbb
supersay245 <samoa4208@protonmail.com>
2021-12-16 09:18:27 -0600

    changes to backend

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 
-const baseUrl = 'production'
-  ? 'https://messageme-api.herokuapp.com/'
-  : 'http://localhost:3000';
+const baseUrl = 'http://localhost:3000';
 
 const api = axios.create({
   baseURL: baseUrl,
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'productive-liquid.surge.sh'
+    origins '*'
 
     resource '*',
       headers: :any,
