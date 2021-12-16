ea6ed7c4e7cd34845880a34a5053d0fb14b4129c
supersay245 <samoa4208@protonmail.com>
2021-12-15 23:52:06 -0600

    cors fix attempt

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'productive-liquid.surge.sh'
 
     resource '*',
       headers: :any,
